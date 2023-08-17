8a6ccfec5cedb3007a8e5f5d89238613e7f076ca
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2023-08-17 08:17:30 -0300

    Recebendo arquivo JSON

---
+++ Servidores/utils.py
@@ -0,0 +1,18 @@
+import json
+
+def extract_route(requisicao):
+    return requisicao.split(" ")[1][1:]
+
+def read_file(path):
+    with open(path, 'rb') as f:
+        resposta = f.read()
+    return resposta
+
+def load_data(nome):
+    with open("data/" + nome, 'r') as arquivo_json:
+        texto = arquivo_json.read()
+    return json.loads(texto)
+
+'''def load_template():
+    return 0
+'''
