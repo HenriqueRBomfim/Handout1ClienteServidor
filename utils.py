f4ced14b3097f5785d20091b73d9e855f002fb9c
HenriqueRBomfim <henriquerb1@al.insper.edu.br>
2023-08-17 10:47:19 -0300

    Quase pronto

--- utils.py
+++ utils.py
@@ -16,3 +16,18 @@ def load_data(nome):
 def load_template(arquivo):
     with open("templates/" + arquivo, 'r') as string:
         return string.read()
+    
+def adiciona(anotacao):
+    with open("data/notes.json", 'w') as arquivo:
+        lista = load_data("notes.json")
+        lista.append(anotacao)
+        novo_json = json.dumps(lista)
+        return arquivo.write(novo_json)
+    
+def build_response(body='', code=200, reason='OK', headers=''):
+    response = ''
+    if headers == '':
+        response = "HTTP/1.1 " + str(code) + " " + reason + headers + "\n\n" + body
+    else:
+        response = "HTTP/1.1 " + str(code) + " " + reason + "\n" + headers + "\n\n" + body
+    return str(response).encode()
--- views.py
+++ views.py
@@ -1,6 +1,27 @@
-from utils import load_data, load_template
+from utils import load_data, load_template, build_response, adiciona
+from urllib.parse import unquote_plus
 
-def index():
+def index(request):
+    # A string de request sempre começa com o tipo da requisição (ex: GET, POST)
+    if request.startswith('POST'):
+        request = request.replace('\r', '')  # Remove caracteres indesejados
+        # Cabeçalho e corpo estão sempre separados por duas quebras de linha
+        partes = request.split('\n\n')
+        corpo = partes[1]
+        params = {}
+        # Preencha o dicionário params com as informações do corpo da requisição
+        # O dicionário conterá dois valores, o título e a descrição.
+        # Posteriormente pode ser interessante criar uma função que recebe a
+        # requisição e devolve os parâmetros para desacoplar esta lógica.
+        # Dica: use o método split da string e a função unquote_plus
+        for chave_valor in corpo.split('&'):
+            separados = chave_valor.split("=")
+            novo_valor = unquote_plus(separados[1])
+            params[separados[0]] = novo_valor
+            adiciona(params)
+
+        return build_response(code=303, reason='See Other', headers='Location: /')
+
     # Cria uma lista de <li>'s para cada anotação
     # Se tiver curiosidade: https://docs.python.org/3/tutorial/datastructures.html#list-comprehensions
     note_template = load_template('components/note.html')
@@ -10,4 +31,6 @@ def index():
     ]
     notes = '\n'.join(notes_li)
 
-    return load_template('index.html').format(notes=notes).encode()
+    body = load_template('index.html').format(notes=notes)
+
+    return build_response(body = body)
